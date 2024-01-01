957d52e3881c0a322c3e369acfc11f0d404ccbee
mrhyper178 <153428922+mrhyper178@users.noreply.github.com>
2024-01-01 17:13:45 +0530

    Update config.py

--- root/config.py
+++ root/config.py
@@ -14,7 +14,7 @@ class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN","5567847114:AAGrO_uulshxfHcqc1X7zygDxahRuyY2C4g")
     AUTH_USERS = [int(x) for x in os.environ.get("AUTH_USERS", "").split()]
     DOWNLOAD_LOCATION = "./bot/DOWNLOADS"
-    DB_URI = os.environ.get("DATABASE_URL","mongodb+srv://leecher:leecher@cluster0.606mkpi.mongodb.net/?retryWrites=true&w=majority")
+    DB_URI = os.environ.get("DATABASE_URL","postgres://ypbpwwzqrntoqn:821ffc75f621cfd2faeea5e95f71224ca773f48a19add6d24c6c1138f021b7db@ec2-23-21-207-93.compute-1.amazonaws.com:5432/dfl6oogvkl08qq")
     # owner is for log cmd only owner can use (this can be multiple users)
     OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID","807374433").split(" ")]
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "BotDunia")
